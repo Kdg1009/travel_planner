9656cad02bdfa6e8f91a88315dad98ae32626962
Kdg1009 <90826434+Kdg1009@users.noreply.github.com>
2025-05-20 22:28:44 +0900

    Update get_pois_from_map.py

--- BackEnd/modules/common/get_pois_from_map.py
+++ BackEnd/modules/common/get_pois_from_map.py
@@ -3,7 +3,8 @@ from modules.common.llm_request import request_to_llm
 from typing import List
 
 # dummy function
-def get_pois_from_map(filter_data:str, location:str)->List[dict]:
+def get_pois_from_map(filter_data:str=None, location:str=None)->List[dict]:
+    # return type has to keep this form
     pois = [
     {"name": "Gyeongbokgung", "latitude": "37.5796", "longitude": "126.9770"},
     {"name": "N Seoul Tower", "latitude": "37.5512", "longitude": "126.9882"}
