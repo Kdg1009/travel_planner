17356c359fe12c206ec0117191bedd29bd87c917
Kdg1009 <90826434+Kdg1009@users.noreply.github.com>
2025-05-20 22:25:13 +0900

    Update llm_request.py

--- BackEnd/modules/common/llm_request.py
+++ BackEnd/modules/common/llm_request.py
@@ -2,6 +2,6 @@ from config import OPENAI_API_KEY
 
 # dummy placeholder for LLM interaction
 
-def request_to_llm(request:str):
+def request_to_llm(request:str)->str:
     return_exmple = "팔공산,32.123,11.3424,56\n경북대,76.45,54.545,44\n이월드,43.434,234.546,33"
     return return_exmple
