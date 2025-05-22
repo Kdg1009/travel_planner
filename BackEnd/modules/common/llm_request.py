6051816c03d20c412efddbe826a5becf546f72c8
Kdg1009 <90826434+Kdg1009@users.noreply.github.com>
2025-05-23 00:36:17 +0900

    Update llm_request.py

--- BackEnd/modules/common/llm_request.py
+++ BackEnd/modules/common/llm_request.py
@@ -9,7 +9,7 @@ def request_to_llm(request:str, client=llm_client)->str:
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system","content": "당신은 여행 일정 전문가입니다."},
+            {"role": "system","content": "You are a travel agency."},
             {"role":"user","content":request}
         ],
         max_tokens=1200,
